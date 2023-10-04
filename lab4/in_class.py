e658674c8d9e3ccedf7199c2b5ed33e3de562f91
Grena30 <grenadinu@yahoo.com>
2023-10-04 19:02:58 +0300

    feat(lab4): add parser for webserver

--- lab4/in_class.py
+++ lab4/in_class.py
@@ -8,7 +8,6 @@ import json2html
 HOST = '127.0.0.1'
 PORT = 8080
 
-# Create a socket that uses IPv4 and TCP
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
