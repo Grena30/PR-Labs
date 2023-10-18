627cbd3d78c064e4fe28290dcbd5d8892df47d9b
Grena30 <grenadinu@yahoo.com>
2023-10-18 20:35:17 +0300

    chore(lab6): remove redundant comments

--- lab6/homework/routes.py
+++ lab6/homework/routes.py
@@ -7,17 +7,13 @@ from __main__ import app
 @app.route('/api/electro-scooters', methods=['POST'])
 def create_electro_scooter():
     try:
-        # Get data from the request body
         data = request.get_json()
 
-        # Validate and extract required parameters
         name = data['name']
         battery_level = data['battery_level']
 
-        # Create a new Electro Scooter
         electro_scooter = ElectroScooter(name=name, battery_level=battery_level)
 
-        # Add the Electro Scooter to the database
         db.session.add(electro_scooter)
         db.session.commit()
 
@@ -29,7 +25,6 @@ def create_electro_scooter():
 
 @app.route('/api/electro-scooters', methods=['GET'])
 def get_electro_scooters():
-    # Get all Electro Scooters from the database
     scooters = ElectroScooter.query.all()
     scooters_list = []
     for scooter in scooters:
@@ -42,7 +37,6 @@ def get_electro_scooters():
 
 @app.route('/api/electro-scooters/<int:scooter_id>', methods=['GET'])
 def get_electro_scooter_by_id(scooter_id):
-    # Find the Electro Scooter by ID
     scooter = ElectroScooter.query.get(scooter_id)
     if scooter is not None:
         return jsonify({
@@ -56,15 +50,12 @@ def get_electro_scooter_by_id(scooter_id):
 @app.route('/api/electro-scooters/<int:scooter_id>', methods=['PUT'])
 def update_electro_scooter(scooter_id):
     try:
-        # Find the Electro Scooter by ID
         scooter = ElectroScooter.query.get(scooter_id)
 
         if scooter is not None:
 
-            # Get data from the request body
             data = request.get_json()
 
-            # Update the Electro Scooter properties
             scooter.name = data.get('name', scooter.name)
             scooter.battery_level = data.get('battery_level', scooter.battery_level)
             db.session.commit()
@@ -81,15 +72,12 @@ def update_electro_scooter(scooter_id):
 @app.route('/api/electro-scooters/<int:scooter_id>', methods=['DELETE'])
 def delete_electro_scooter(scooter_id):
     try:
-        # Find the Electro Scooter by ID
         scooter = ElectroScooter.query.get(scooter_id)
         if scooter is not None:
 
-            # Get the password from the request headers
             password = request.headers.get('X-Delete-Password')
 
-            # Check if the provided password is correct
-            if password == 'pass':  # Replace with your actual password
+            if password == 'pass':
                 db.session.delete(scooter)
                 db.session.commit()
                 return jsonify({"message": "Electro Scooter deleted successfully"}), 200
